259d59fdd9c9a1ca2f436094005299bc135ddcc4
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2022-01-28 21:06:10 +0200

    feat: break app by adding BrowserAnimationsModule + APP_INITIALIZER + RendererFactory2 dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule, RendererFactory2} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
+
+function initializeAppFactory() {
+  return () => {};
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      deps: [RendererFactory2],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
